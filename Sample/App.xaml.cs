32ad178d3b26e0ecd054fc9efa1912afea916fbb
Michael Ch (DevExpress) <45622253+MichaelDX@users.noreply.github.com>
2021-02-26 13:43:27 +0300

    Add files via upload

---
+++ Sample/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sample {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            //CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("hr-BA");
+            //CultureInfo culture = CultureInfo.CreateSpecificCulture("ru-RU");
+            Thread.CurrentThread.CurrentUICulture = culture;
+            Thread.CurrentThread.CurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Sample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace Sample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        List<string> list = new List<string>();
+
+        public MainWindow() {
+            InitializeComponent();          
+            for (int i = 0; i < 1000000; i++) {
+                list.Add($"{i}");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var watch = new Stopwatch();
+            watch.Start();
+            list.ForEach(str => str.StartsWith("1"));
+            watch.Stop();
+
+            MessageBox.Show($"Culture: {Thread.CurrentThread.CurrentCulture.Name}; Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
